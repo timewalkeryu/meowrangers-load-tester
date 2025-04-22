ac390cb75e347f1db3a49233493d94c503e4b7ef
HyeonGyu Yu <timewalker.yu@layerlab.io>
2025-04-22 18:09:03 +0900

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -14,7 +14,7 @@ HTTP_MAX_CONNECTIONS = 100  # 최대 동시 연결 수
 HTTP_VERIFY_SSL = False  # SSL 검증 여부
 
 # API 호출 제한 설정
-API_SEMAPHORE_LIMIT = 300  # API 호출에 사용할 세마포어 제한
+API_SEMAPHORE_LIMIT = 100  # API 호출에 사용할 세마포어 제한
 
 # 테스트 설정
 SERVER_ALIAS = "SEOUL-001"
